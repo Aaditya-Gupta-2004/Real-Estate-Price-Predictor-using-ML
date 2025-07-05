d7d1414adae31139378c5e893fe08418ac08b2e8
Aaditya <>
2025-07-05 23:47:11 +0530

    added frontend

--- BHP/server/server.py
+++ BHP/server/server.py
@@ -18,12 +18,12 @@ def get_location_names():
 @app.route("/predict_home_price",methods=['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
-    location_price = request.form['location_price']
+    location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
 
     response = jsonify({
-        "estimated_price": utils.predict_estimate_price(total_sqft,location_price,bhk,bath)
+        "estimated_price": utils.predict_estimate_price(total_sqft,location,bhk,bath)
     })
 
     response.headers.add('Access-Control-Allow-Origin','*')
